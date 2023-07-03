5985b947f2712f89fad9ca7ef80cf26697a434fe
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-07-03 16:43:32 +0530

    implement Header and Footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <span>
+        <i className='fa-solid fa-code'></i> DROPDOWN
+      </span>
+    </div>
+  );
+};
+
+export default Header;
